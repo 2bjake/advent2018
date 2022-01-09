039b991e894110b6bcdb5fe86e25080afb2ef204
Jake <2bjake@users.noreply.github.com>
2022-01-09 05:36:52 -0600

    Initial commit

---
+++ Sources/Main/Runner.swift
@@ -0,0 +1,20 @@
+import Day1
+import Extensions
+
+@main
+struct Runner {
+  static func main() {
+    partOne()
+    partTwo()
+
+    //printMillisElapsed("partOne", for: partOne)
+    //printAvgMillisElapsed("partTwo", runTimes: 100, for: partTwo)
+  }
+
+//  static func main() async throws {
+//    await partOne()
+//    await partTwo()
+//  }
+}
+
+
